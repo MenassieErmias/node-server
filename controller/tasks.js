04ee962f6bde9e7b7ea603527e7d60f9cae5e08d
MenassieErmias <ermiasmenase@gmail.com>
2024-11-20 15:37:28 +0300

    createTasks function added: controller/tasks.js

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,16 +1,26 @@
 import v4 from 'uuid';
 
-const tasks = [
-  {
-    name: 'Menassie',
-    todo: 'Something'
-  }
+let tasks = [
+
 ];
 
+// Get all tasks
 const getAllTasks = (req, res) => {
   res.status(200).json(tasks);
 }
 
 const createTasks = (req, res) => {
+  const { name, time, endTime, complete: false} = req.body;
+
+  const newTask = {
+    id: v4(),
+    name: name,
+    time: time,
+    endTime: endTime,
+    complete: false
+  }
+
+  tasks = [...tasks, newTask];
 
 }
+
